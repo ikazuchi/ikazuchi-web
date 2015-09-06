38d83eb406a8d4047f4862380cafce5d1056e4ef
awkwardusername <awkwardusername@gmail.com>
2015-09-06 09:36:53 +0800

    edit alerts api

--- app/Alert.php
+++ app/Alert.php
@@ -6,6 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Alert extends Model
 {
+    protected $guarded = ['id', 'created_at', 'updated_at'];
+
     public function client() {
         return $this->belongsTo(Client::class);
     }
